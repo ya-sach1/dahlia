7644be5d303d146c5cb351c3000d09b2a1fdb217
ya-sach1 <ikurogane85@gmail.com>
2021-06-07 22:10:06 +0200

    🔥 Drop support for JSON because fuck you

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,56 @@
-import { parse as parseYAML, stringify as stringifyYAML } from 'yaml';
+import { parse, stringify } from 'yaml';
 import { readFileSync, statSync, writeFileSync } from 'fs';
 import { get, has, set, delete as dotDelete } from 'dot-prop';
-import { extname } from 'path';
-
-type AvaiableTypes = 'json' | 'yml' | 'yaml';
-
-class Parser {
-	public parse: (...args: any[]) => Record<string, any>;
-	public stringify: (...args: any[]) => string;
-
-	public constructor(type: string | AvaiableTypes) {
-		switch (type) {
-			case 'yaml':
-				this.parse = parseYAML;
-				this.stringify = stringifyYAML;
-				break;
-			case 'yml':
-				this.parse = parseYAML;
-				this.stringify = stringifyYAML;
-				break;
-			case 'json':
-				this.parse = JSON.parse;
-				this.stringify = JSON.stringify;
-				break;
-			default:
-				this.parse = JSON.parse;
-				this.stringify = JSON.stringify;
-				break;
-		}
-	}
-}
 
 export class Configuration {
-	private parsed: Record<string, any> = {};
-	private parser: Parser;
+	private _parsed: Record<string, unknown> = {};
 
-	public constructor(private readonly path: string, type?: string | AvaiableTypes) {
+	/**
+	 * Instantiates a configuration
+	 * @param {string} path Path to config file
+	 */
+	constructor(private readonly path: string) {
 		if (!statSync(path)) throw new Error("Couldn't parse the config file; is the path correct? does the file exist?");
 
-		this.parser = new Parser(type ?? extname(path).slice(1));
-		this.parse();
+		this._parse();
 	}
 
-	// I don't feel like ruining code-style just to return directly with arrow functions
+	private _parse(): void {
+		this._parsed = parse(readFileSync(this.path, { encoding: 'utf-8' }));
+	}
 
-	private parse = (): void => {
-		this.parsed = this.parser.parse(readFileSync(this.path, { encoding: 'utf-8' }));
-	};
+	/**
+	 * Get a value from specified path
+	 */
+	get<T>(path: string, defaultValue?: T): T | undefined {
+		return get<T>(this._parsed, path) ?? defaultValue;
+	}
 
-	public get<T>(path: string, defaultValue?: T): T | undefined {
-		return get<T>(this.parsed, path) ?? defaultValue;
-	};
+	/**
+	 * Check if the path exists
+	 */
+	has(path: string): boolean {
+		return has(this._parsed, path);
+	}
 
-	public has = (path: string): boolean => {
-		return has(this.parsed, path);
-	};
+	/**
+	 * Set value at specified path
+	 */
+	set(path: string, value: unknown): this {
+		writeFileSync(this.path, stringify(set(this._parsed, path, value)));
 
-	public set = (path: string, value: unknown): this => {
-		writeFileSync(this.path, this.parser.stringify(set(this.parsed, path, value)));
-
-		this.parse();
+		this._parse();
 		return this;
-	};
+	}
 
-	public delete = (path: string): this => {
-		dotDelete(this.parsed, path);
-		writeFileSync(this.path, this.parser.stringify(this.parsed));
+	/**
+	 * Delete value at specified path
+	 */
+	delete(path: string): this {
+		dotDelete(this._parsed, path);
+		writeFileSync(this.path, stringify(this._parsed));
 
-		this.parse();
+		this._parse();
 		return this;
-	};
+	}
 }
